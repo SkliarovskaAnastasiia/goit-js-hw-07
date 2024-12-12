9b86f6ae3f7cd6058489db80b6523d7934c06762
Anastasiia Skliarovska <skliarovskaa@gmail.com>
2024-12-12 13:21:14 +0200

    did task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector('.js-login-form');
 
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const userEmail = formEl.elements.email.value.trim();
+  const userPassword = formEl.elements.password.value.trim();
+
+  if (userEmail === '' || userPassword === '') {
+    return alert('All form fields must be filled in');
+  }
+
+  const formData = {
+    [formEl.elements.email.name]: userEmail,
+    [formEl.elements.password.name]: userPassword,
+  };
+
+  console.log(formData);
+
+  formEl.reset();
+});
